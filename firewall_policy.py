6c211ccd94ac4e316f7051332b50a9e9b0ebe21b
Nick Lee <nicklee@gatech.edu>
2016-02-06 21:05:32 -0800

    I changed the code to allow for intersections within the same lines of conditions
    This is more robust.

--- firewall_policy.py
+++ firewall_policy.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+﻿#!/usr/bin/python
 															
 "Project 2 - This creates the firewall policy. "
 
@@ -10,7 +10,7 @@ from pyretic.core import packet
 
 def make_firewall_policy(config):
     # TODO - This is where you need to write the functionality to create the
-    # firewall. What is passed in is a list of rules that you must implement.
+    # firewall. What is passed in is a list of rules that you must  implement.
     
     # feel free to remove the following "print config" line once you no longer need it
     print config # for demonstration purposes only, so you can see the format of the config
@@ -20,24 +20,33 @@ def make_firewall_policy(config):
         # TODO - build the individual rules
 
         # examples:
+        rule_queue = [] #clean up the rule_queue
 
         #check for '*' wildcard to avoid crashing the parser.
         if entry['dstport']!='*':
             rule = match(dstport=int(entry['dstport']), ethtype=packet.IPV4, protocol=packet.TCP_PROTO)
+            rule_queue.append(rule)
         if entry['srcmac']!='*':
             rule = match(srcmac=MAC(entry['srcmac']))
+            rule_queue.append(rule)
         if entry['srcip']!='*':
             rule = match(srcip=entry['srcip'])
+            rule_queue.append(rule)
+        if entry['dstip']!='*':
+            rule = match(dstip=entry['dstip'])
+            rule_queue.append(rule)
         if entry['dstmac']!='*':
             rule = match(dstmac=MAC(entry['dstmac']))
+            rule_queue.append(rule)
         if entry['srcport']!='*':
             rule = match(srcport=int(entry['srcport']), ethtype=packet.IPV4, protocol=packet.TCP_PROTO)
+            rule_queue.append(rule)
 
-        rules.append(rule)
+
+        rules.append(intersection(rule_queue)) #theoretically this should work
         pass
     
     allowed = ~(union(rules))
     print allowed
 
     return allowed
-
